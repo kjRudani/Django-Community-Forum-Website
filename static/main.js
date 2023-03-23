7e1526bdb6a18100cebb27fb691c7bf20704e8cc
kjRudani <krishna.rudani@somaiya.edu>
2023-03-23 22:49:43 +0530

    made new navbar

--- static/main.js
+++ static/main.js
@@ -1,4 +1,14 @@
 //NavBar
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+  }
+  
+
 function hideIconBar(){
     var iconBar = document.getElementById("iconBar");
     var navigation = document.getElementById("navigation");
@@ -24,3 +34,36 @@ function showReplies(id){
     var replyArea = document.getElementById(id);
     replyArea.classList.remove("hide");
 }
+
+
+(function($) { 
+    $(function() { 
+  
+      //  open and close nav 
+      $('#navbar-toggle').click(function() {
+        $('nav ul').slideToggle();
+      });
+  
+  
+      // Hamburger toggle
+      $('#navbar-toggle').on('click', function() {
+        this.classList.toggle('active');
+      });
+  
+  
+      // If a link has a dropdown, add sub menu toggle.
+      $('nav ul li a:not(:only-child)').click(function(e) {
+        $(this).siblings('.navbar-dropdown').slideToggle("slow");
+  
+        // Close dropdown when select another dropdown
+        $('.navbar-dropdown').not($(this).siblings()).hide("slow");
+        e.stopPropagation();
+      });
+  
+  
+      // Click outside the dropdown will remove the dropdown class
+      $('html').click(function() {
+        $('.navbar-dropdown').hide();
+      });
+    }); 
+  })(jQuery); 
